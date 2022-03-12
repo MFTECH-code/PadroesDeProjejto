88df14a7e0800bac5264f00754a56eabe6ed7455
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-03-12 15:12:34 -0300

    Criação da entidade produto

---
+++ MainProjeect/Entities/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProjeect.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public TipoDeProduto Tipo { get; set; }
+    }
+
+    public enum TipoDeProduto
+    {
+        Eletronico = 1,
+        EletroDomestico = 2,
+        Alimenticio = 3
+    }
+}
