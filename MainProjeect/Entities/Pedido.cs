ebec7652b6b92469f9c4de010ae3135cb22f4ad9
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-03-12 21:17:37 -0300

    Criação da classe Pedido

---
+++ MainProjeect/Entities/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProjeect.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public IList<Pedido> Items { get; set; }
+        public decimal ValorTotal { get; set; }
+        public StatusPedido Status { get; set; }
+    }
+
+    public enum StatusPedido
+    {
+        Procesamento = 1,
+        Pago = 2,
+        Enviado = 3,
+        Entregue = 4
+    }
+}
